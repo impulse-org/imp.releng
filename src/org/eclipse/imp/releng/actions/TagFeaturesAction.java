b520f0bb638ab97d8220d20588859a96e91fbbf4
Robert M. Fuhrer <rmfuhrer@optonline.net>
2008-03-08 00:13:37 +0000

    Now calls new API on the ReleaseTool to do the tagging.

--- src/org/eclipse/imp/releng/actions/TagFeaturesAction.java
+++ src/org/eclipse/imp/releng/actions/TagFeaturesAction.java
@@ -1,9 +1,8 @@
 package org.eclipse.imp.releng.actions;
 
+import org.eclipse.imp.releng.WorkbenchReleaseTool;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.team.internal.ccvs.core.CVSTag;
-import org.eclipse.team.internal.ccvs.ui.operations.TagOperation;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
@@ -16,15 +15,6 @@ public class TagFeaturesAction implements IWorkbenchWindowActionDelegate {
     public void selectionChanged(IAction action, ISelection selection) { }
 
     public void run(IAction action) {
-        // For inspiration, see org.eclipse.releng.tools.TagAndReleaseOperation
-        //
-        // N.B. For SVN, which doesn't support tags, this may have to copy the
-        // given folder to a name that embeds the desired tag, and set the SVN
-        // "final" property on the resulting folder.
-        //
-        String featureID= "org.eclipse.imp.runtime";
-        String featureVersion= "3.2.72";
-        CVSTag tag= new CVSTag(featureID + "-" + featureVersion, CVSTag.VERSION);
-        TagOperation tagOp= null; // new TagOperation();
+        new WorkbenchReleaseTool().tagFeatures();
     }
 }
