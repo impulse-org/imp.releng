ec16ee05df6aeed655d65571693a00484f25d540
Robert M. Fuhrer <rmfuhrer@optonline.net>
2008-05-12 22:43:15 +0000

    Added checkboxes so that user can request also retrieving the features and plugins contained by the update site in one shot (rather than requiring the user to go through 3 levels of dialogs).

--- src/org/eclipse/imp/releng/dialogs/RetrieveUpdateSiteDialog.java
+++ src/org/eclipse/imp/releng/dialogs/RetrieveUpdateSiteDialog.java
@@ -1,10 +1,15 @@
 package org.eclipse.imp.releng.dialogs;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.imp.releng.ReleaseTool;
 import org.eclipse.imp.releng.WorkbenchReleaseTool;
+import org.eclipse.imp.releng.metadata.FeatureInfo;
+import org.eclipse.imp.releng.metadata.UpdateSiteInfo;
+import org.eclipse.imp.releng.metadata.UpdateSiteInfo.FeatureRef;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.SWT;
@@ -29,7 +34,9 @@ public class RetrieveUpdateSiteDialog extends Dialog {
 	private String fProvider;
 	private String fRepoServer;
 	private String fRepoPath;
-	private String fProjectName;
+	private String fUpdateSiteProjectName;
+	private boolean fRetrieveFeatures;
+	private boolean fRetrievePlugins;
 
 	public RetrieveUpdateSiteDialog(Shell shell) {
         super(shell);
@@ -49,11 +56,11 @@ public class RetrieveUpdateSiteDialog extends Dialog {
     	public String updateSiteProject;
 
     	public PrefabDescriptor(String pt, String rs, String rp, String usp) {
-    		providerType= pt;
-    		repoServer= rs;
-    		repoPath= rp;
-    		updateSiteProject= usp;
-		}
+    	    providerType= pt;
+    	    repoServer= rs;
+    	    repoPath= rp;
+    	    updateSiteProject= usp;
+    	}
     }
 
     private final static Map<String, PrefabDescriptor> sUpdateSiteMap= new HashMap<String, PrefabDescriptor>();
@@ -72,22 +79,45 @@ public class RetrieveUpdateSiteDialog extends Dialog {
         createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
 
         proceedButton.addSelectionListener(new SelectionListener() {
-			public void widgetDefaultSelected(SelectionEvent e) { }
-			public void widgetSelected(SelectionEvent e) {
-				String providerRef= sProviderMap.get(fProvider);
-				String projectRef= buildProjectRef(fProvider, fRepoServer, fRepoPath, fProjectName);
-				new WorkbenchReleaseTool().retrieveProject(projectRef, providerRef);
-			}
-			private String buildProjectRef(String providerType, String repoServer, String repoPath, String projectName) {
-				if (providerType.equals("CVS")) {
-					// 1.0,:extssh:orquesta.watson.ibm.com:/usr/src/cvs/JDT,com.ibm.watson.demo.ecoop2005.JLex,com.ibm.watson.demo.ecoop2005.JLex"
-					return "1.0,:extssh:" + repoServer + ":" + repoPath + "," + projectName + "," + projectName;
-				} else if (providerType.equals("SVN")) {
-					// "0.9.3,https://dev.eclipse.org/svnroot/technology/org.eclipse.imp/org.eclipse.imp.update/trunk,org.eclipse.imp.update"
-					return "0.9.3,https://" + repoServer + repoPath + "/" + projectName + "/trunk," + projectName;
-				}
-				throw new IllegalArgumentException("huh?");
-			}
+            public void widgetDefaultSelected(SelectionEvent e) { }
+            public void widgetSelected(SelectionEvent e) {
+                String providerRef= sProviderMap.get(fProvider);
+                String updateSiteProjectRef= buildProjectRef(fProvider, fRepoServer, fRepoPath, fUpdateSiteProjectName);
+                WorkbenchReleaseTool releaseTool = new WorkbenchReleaseTool();
+
+                releaseTool.retrieveProject(updateSiteProjectRef, providerRef);
+
+                releaseTool.collectMetaData(true); // Now that we have the update site, rescan the metadata to collect the set of features
+                if (fRetrieveFeatures) {
+                    List<UpdateSiteInfo> siteInfos= new ArrayList<UpdateSiteInfo>();
+                    UpdateSiteInfo siteInfo= releaseTool.findSiteByName(fUpdateSiteProjectName);
+                    siteInfos.add(siteInfo);
+
+                    if (!releaseTool.retrieveFeatures(siteInfos)) {
+                        return;
+                    }
+
+                    releaseTool.collectMetaData(true); // Now that we have the features, rescan the metadata to collect the set of plugins
+                    if (fRetrievePlugins) {
+                        List<FeatureInfo> featureInfos= new ArrayList<FeatureInfo>();
+
+                        for(FeatureRef featureRef: siteInfo.fFeatureRefs) {
+                            featureInfos.add(releaseTool.findFeatureInfo(featureRef.getID()));
+                        }
+                        releaseTool.retrievePlugins(featureInfos);
+                    }
+                }
+            }
+            private String buildProjectRef(String providerType, String repoServer, String repoPath, String projectName) {
+                if (providerType.equals("CVS")) {
+                    // 1.0,:extssh:orquesta.watson.ibm.com:/usr/src/cvs/JDT,com.ibm.watson.demo.ecoop2005.JLex,com.ibm.watson.demo.ecoop2005.JLex"
+                    return "1.0,:extssh:" + repoServer + ":" + repoPath + "," + projectName + "," + projectName;
+                } else if (providerType.equals("SVN")) {
+                    // "0.9.3,https://dev.eclipse.org/svnroot/technology/org.eclipse.imp/org.eclipse.imp.update/trunk,org.eclipse.imp.update"
+                    return "0.9.3,https://" + repoServer + repoPath + "/" + projectName + "/trunk," + projectName;
+                }
+                throw new IllegalArgumentException("huh?");
+            }
         });
     }
 
@@ -99,12 +129,11 @@ public class RetrieveUpdateSiteDialog extends Dialog {
     	fRepoServerText.setText(pfd.repoServer);
     	fRepoPathText.setText(pfd.repoPath);
     	fProjectNameText.setText(pfd.updateSiteProject);
-    	this.dialogArea.pack();
-	}
+    }
 
     @Override
     protected Control createDialogArea(Composite parent) {
-        Composite area= (Composite) super.createDialogArea(parent);
+        final Composite area= (Composite) super.createDialogArea(parent);
         GridLayout grid= new GridLayout(2, true);
         area.setLayout(grid);
 
@@ -117,13 +146,13 @@ public class RetrieveUpdateSiteDialog extends Dialog {
         prefabCombo.add("Custom");
 
         prefabCombo.addSelectionListener(new SelectionListener() {
-			public void widgetDefaultSelected(SelectionEvent e) { }
+            public void widgetDefaultSelected(SelectionEvent e) { }
 
-			public void widgetSelected(SelectionEvent e) {
-				fillPrefabValues(prefabCombo.getItem(prefabCombo.getSelectionIndex()));
-			}
+            public void widgetSelected(SelectionEvent e) {
+                fillPrefabValues(prefabCombo.getItem(prefabCombo.getSelectionIndex()));
+                area.pack();
+            }
         });
-        prefabCombo.select(0);
 
         Label providerLabel= new Label(area, SWT.NONE);
         fProviderCombo= new Combo(area, SWT.DROP_DOWN);
@@ -131,23 +160,21 @@ public class RetrieveUpdateSiteDialog extends Dialog {
         fProviderCombo.add("CVS");
         fProviderCombo.add("SVN");
         fProviderCombo.addSelectionListener(new SelectionListener() {
-			public void widgetDefaultSelected(SelectionEvent e) { }
+            public void widgetDefaultSelected(SelectionEvent e) { }
 
-			public void widgetSelected(SelectionEvent e) {
-				fProvider= fProviderCombo.getItem(fProviderCombo.getSelectionIndex());
-			}
+            public void widgetSelected(SelectionEvent e) {
+                fProvider= fProviderCombo.getItem(fProviderCombo.getSelectionIndex());
+            }
         });
-        fProviderCombo.select(1); // start with SVN
-        fProvider= "SVN";
 
         Label repoURLLabel= new Label(area, SWT.NONE);
         repoURLLabel.setText("Repository server:");
         fRepoServerText= new Text(area, SWT.NONE);
         fRepoServerText.setText("dev.eclipse.org");
         fRepoServerText.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				fRepoServer= fRepoServerText.getText();
-			}
+            public void modifyText(ModifyEvent e) {
+                fRepoServer= fRepoServerText.getText();
+            }
         });
 
         Label repoPathLabel= new Label(area, SWT.NONE);
@@ -155,9 +182,9 @@ public class RetrieveUpdateSiteDialog extends Dialog {
         fRepoPathText= new Text(area, SWT.NONE);
         fRepoPathText.setText("/svnroot/technology/org.eclipse.imp");
         fRepoPathText.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				fRepoPath= fRepoPathText.getText();
-			}
+            public void modifyText(ModifyEvent e) {
+                fRepoPath= fRepoPathText.getText();
+            }
         });
 
         Label updateProjectLabel= new Label(area, SWT.NONE);
@@ -165,11 +192,42 @@ public class RetrieveUpdateSiteDialog extends Dialog {
         fProjectNameText= new Text(area, SWT.NONE);
         fProjectNameText.setText("org.eclipse.imp.update");
         fProjectNameText.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				fProjectName= fProjectNameText.getText();
-			}
+            public void modifyText(ModifyEvent e) {
+                fUpdateSiteProjectName= fProjectNameText.getText();
+            }
         });
-        
+
+        final Button retrieveFeaturesCkbox= new Button(area, SWT.CHECK);
+        retrieveFeaturesCkbox.setText("Also retrieve feature projects");
+        retrieveFeaturesCkbox.addSelectionListener(new SelectionListener() {
+            public void widgetDefaultSelected(SelectionEvent e) { }
+            public void widgetSelected(SelectionEvent e) {
+                setRetrieveFeatures(retrieveFeaturesCkbox.getSelection());
+            }
+        });
+
+        /*Label dummy=*/ new Label(area, SWT.NONE);
+
+        final Button retrievePluginsCkbox= new Button(area, SWT.CHECK);
+        retrievePluginsCkbox.setText("Also retrieve plugin projects");
+        retrievePluginsCkbox.addSelectionListener(new SelectionListener() {
+            public void widgetDefaultSelected(SelectionEvent e) { }
+            public void widgetSelected(SelectionEvent e) {
+                setRetrievePlugins(retrievePluginsCkbox.getSelection());
+            }
+        });
+
+        prefabCombo.select(0);
+        fillPrefabValues("IMP");
+        area.pack();
         return area;
+    }
+
+    protected void setRetrieveFeatures(boolean yesNo) {
+        fRetrieveFeatures= yesNo;
+    }
+
+    protected void setRetrievePlugins(boolean yesNo) {
+        fRetrievePlugins= yesNo;
     }
 }
