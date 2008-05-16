7281f3df11e10fb3d491453339e0353972ecbcf5
Robert M. Fuhrer <rmfuhrer@optonline.net>
2008-05-16 21:56:26 +0000

    Added support for a "Build Release" action.

---
+++ src/org/eclipse/imp/releng/actions/BuildReleaseAction.java
@@ -0,0 +1,20 @@
+package org.eclipse.imp.releng.actions;
+
+import org.eclipse.imp.releng.WorkbenchReleaseTool;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+public class BuildReleaseAction implements IWorkbenchWindowActionDelegate {
+
+    public void dispose() { }
+
+    public void init(IWorkbenchWindow window) { }
+
+    public void run(IAction action) {
+        new WorkbenchReleaseTool().buildRelease();
+    }
+
+    public void selectionChanged(IAction action, ISelection selection) { }
+}
