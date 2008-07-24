a89cae36cd89748e1675b51cfd004a88e7c27d0d
Robert M. Fuhrer <rmfuhrer@optonline.net>
2008-07-24 19:57:09 +0000

    Fixed indentation.

--- src/org/eclipse/imp/releng/metadata/FeatureInfo.java
+++ src/org/eclipse/imp/releng/metadata/FeatureInfo.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
-* Copyright (c) 2007 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
 
-*******************************************************************************/
+ *******************************************************************************/
 
 package org.eclipse.imp.releng.metadata;
 
@@ -30,11 +30,10 @@ import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 /**
- * Simple class that records key XML metadata about a feature, including its manifest
- * file (as an XML document), the set of required features (represented by the XML
- * document nodes for the corresponding "import" elements), and the set of
- * plugins contained in the feature (represented by the XML document nodes for the
- * corresponding "plugin" element).
+ * Simple class that records key XML metadata about a feature, including its manifest file (as an XML document), the set of required features (represented by
+ * the XML document nodes for the corresponding "import" elements), and the set of plugins contained in the feature (represented by the XML document nodes for
+ * the corresponding "plugin" element).
+ * 
  * @author rfuhrer
  */
 public class FeatureInfo {
@@ -50,14 +49,12 @@ public class FeatureInfo {
     public Document fManifestDoc;
 
     /**
-     * The set of required features, represented by the XML
-     * document nodes for the corresponding "import" elements.
+     * The set of required features, represented by the XML document nodes for the corresponding "import" elements.
      */
     public Set<Node> fRequiredFeatures= new HashSet<Node>();
 
     /**
-     * The set of plugins contained in the feature (represented by the
-     * XML document nodes for the corresponding "plugin" element.
+     * The set of plugins contained in the feature (represented by the XML document nodes for the corresponding "plugin" element.
      */
     public Set<Node> fPlugins= new HashSet<Node>();
 
@@ -68,8 +65,8 @@ public class FeatureInfo {
     private Node fDescriptionNode;
 
     public FeatureInfo(IProject project, IFile manifestFile) throws ParserConfigurationException, SAXException, IOException {
-	fProject= project;
-	fManifestFile= manifestFile;
+        fProject= project;
+        fManifestFile= manifestFile;
 
         ReleaseEngineeringPlugin.getMsgStream().println("Got manifest document for " + manifestFile.getLocation().toPortableString());
 
@@ -77,7 +74,7 @@ public class FeatureInfo {
         DocumentBuilder docBuilder= factory.newDocumentBuilder();
 
         fManifestDoc= docBuilder.parse(new File(manifestFile.getLocation().toOSString()));
-	fFeatureID= getFeatureNode().getAttributes().getNamedItem("id").getNodeValue();
+        fFeatureID= getFeatureNode().getAttributes().getNamedItem("id").getNodeValue();
 
         Node featureNode= getFeatureNode();
         Node versionNode= featureNode.getAttributes().getNamedItem("version");
@@ -97,23 +94,27 @@ public class FeatureInfo {
         Node featureNode= getFeatureNode();
         String featureVersion= featureNode.getAttributes().getNamedItem("version").getNodeValue();
 
-        ReleaseEngineeringPlugin.getMsgStream().println("Processing feature " + featureNode.getAttributes().getNamedItem("id") + " version " + featureVersion + " from " + fManifestFile.getLocation().toPortableString());
+        ReleaseEngineeringPlugin.getMsgStream().println(
+                "Processing feature " + featureNode.getAttributes().getNamedItem("id") + " version " + featureVersion + " from "
+                        + fManifestFile.getLocation().toPortableString());
 
         Set<Node> featureRequires= ReleaseTool.getChildNodesNamed("requires", featureNode);
         Set<Node> featurePlugins= ReleaseTool.getChildNodesNamed("plugin", featureNode);
 
-        for(Node required: featureRequires) {
+        for(Node required : featureRequires) {
             Set<Node> imports= ReleaseTool.getChildNodesNamed("import", required);
-            for(Node imprt: imports) {
+            for(Node imprt : imports) {
                 if (imprt.getAttributes().getNamedItem("feature") != null) {
-                    ReleaseEngineeringPlugin.getMsgStream().println("   requires " + imprt.getAttributes().getNamedItem("feature") + " version " + imprt.getAttributes().getNamedItem("version"));
+                    ReleaseEngineeringPlugin.getMsgStream().println(
+                            "   requires " + imprt.getAttributes().getNamedItem("feature") + " version " + imprt.getAttributes().getNamedItem("version"));
                     fRequiredFeatures.add(imprt);
                 } else if (imprt.getAttributes().getNamedItem("plugin") != null) {
-//                    VersionIncrementerPlugin.getMsgStream().println("   requires " + imprt.getAttributes().getNamedItem("plugin") + " version " + imprt.getAttributes().getNamedItem("version"));
+                    // VersionIncrementerPlugin.getMsgStream().println(" requires " + imprt.getAttributes().getNamedItem("plugin") + " version " +
+                    // imprt.getAttributes().getNamedItem("version"));
                 }
             }
         }
-        for(Node plugin: featurePlugins) {
+        for(Node plugin : featurePlugins) {
             String pluginID= plugin.getAttributes().getNamedItem("id").getNodeValue();
             String pluginVersion= plugin.getAttributes().getNamedItem("version").getNodeValue();
 
@@ -140,7 +141,8 @@ public class FeatureInfo {
 
     @Override
     public boolean equals(Object obj) {
-        if (!(obj instanceof FeatureInfo)) return false;
+        if (!(obj instanceof FeatureInfo))
+            return false;
         FeatureInfo other= (FeatureInfo) obj;
         return fFeatureID.equals(other.fFeatureID);
     }
